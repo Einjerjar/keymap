09ba2d493a7db5a5d101e6e05db7ce390d3867b0
Einjerjar <10793955+Einjerjar@users.noreply.github.com>
2022-03-24 13:57:01 +0800

    override screen on 1.17

--- src/main/java/com/github/einjerjar/mc/keymap/mixin/OptionsScreenMixin.java
+++ src/main/java/com/github/einjerjar/mc/keymap/mixin/OptionsScreenMixin.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,12 +14,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
-@Mixin(value = ControlsOptionsScreen.class)
-public class ControlsOptionsScreenMixin {
-    @Inject(at = @At("HEAD"), method = "method_19872", cancellable = true)
+@Mixin(value = OptionsScreen.class)
+public class OptionsScreenMixin {
+    @Inject(at = @At("HEAD"), method = "method_19827", cancellable = true)
     private void openKeybindScreen(ButtonWidget button, CallbackInfo ci) {
         if (KeymapMain.cfg.replaceKeybindScreen) {
-            MinecraftClient.getInstance().setScreen(new KeymappingScreen((ControlsOptionsScreen) (Object) this));
+            MinecraftClient.getInstance().setScreen(new KeymappingScreen((OptionsScreen) (Object) this));
             ci.cancel();
         }
     }
