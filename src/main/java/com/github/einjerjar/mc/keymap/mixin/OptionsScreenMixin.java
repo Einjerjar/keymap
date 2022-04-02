312823befe79e64497bf2239470a02b37acfc040
Einjerjar <10793955+Einjerjar@users.noreply.github.com>
2022-04-02 15:12:08 +0800

    typo from merge

--- src/main/java/com/github/einjerjar/mc/keymap/mixin/OptionsScreenMixin.java
+++ src/main/java/com/github/einjerjar/mc/keymap/mixin/OptionsScreenMixin.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,7 +20,7 @@ public class OptionsScreenMixin {
     private void openKeybindScreen(ButtonWidget button, CallbackInfo ci) {
         if (KeymapMain.cfg.replaceKeybindScreen) {
             // MinecraftClient.getInstance().setScreen(new TestingScreen((Screen) (Object) this));
-            MinecraftClient.getInstance().setScreen(new KeymappingScreen2((OptionsScreen) (Object) this));
+            MinecraftClient.getInstance().setScreen(new KeyMappingScreen2((Screen) (Object) this));
             ci.cancel();
         }
     }
