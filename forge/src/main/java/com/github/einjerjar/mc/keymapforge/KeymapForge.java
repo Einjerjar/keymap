5ca1164c9e97de58c319c037a5ae0c131f0eeebc
Ein <10793955+Einjerjar@users.noreply.github.com>
2022-07-01 11:48:52 +0800

    forgot to add mod menu version, also forge config

--- forge/src/main/java/com/github/einjerjar/mc/keymapforge/KeymapForge.java
+++ forge/src/main/java/com/github/einjerjar/mc/keymapforge/KeymapForge.java
@@ -10,10 +10,10 @@ import com.github.einjerjar.mc.keymapforge.client.gui.screen.ConfigScreen;
 import com.github.einjerjar.mc.keymapforge.cross.CrossKeybind;
 import com.github.einjerjar.mc.keymapforge.cross.IntegrationRegistrar;
 import com.github.einjerjar.mc.keymapforge.cross.TickEventRegistrar;
-import net.minecraftforge.client.ConfigGuiHandler;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fmlclient.ConfigGuiHandler;
 
 import java.io.File;
 
@@ -29,7 +29,8 @@ public class KeymapForge {
         Keymap.init();
 
         ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class,
-                () -> new ConfigGuiHandler.ConfigGuiFactory((minecraft, screen) -> ConfigScreenShared.provider().apply(screen)));
+                () -> new ConfigGuiHandler.ConfigGuiFactory((minecraft, screen) -> ConfigScreenShared.provider().apply(
+                        screen)));
     }
 
     public static File configDirProvider() {
