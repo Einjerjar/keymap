313fa0c6dd6ec4a5d0e7df69907ba870fa60af57
Ein <10793955+Einjerjar@users.noreply.github.com>
2022-07-01 12:33:04 +0800

    1.19 text, again

--- common/src/main/java/com/github/einjerjar/mc/widgets/utils/Text.java
+++ common/src/main/java/com/github/einjerjar/mc/widgets/utils/Text.java
@@ -1,8 +1,7 @@
 package com.github.einjerjar.mc.widgets.utils;
 
 import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.Component;
 
 public class Text {
 
@@ -10,10 +9,10 @@ public class Text {
     }
 
     public static MutableComponent literal(String s) {
-        return new TextComponent(s);
+        return Component.literal(s);
     }
 
     public static MutableComponent translatable(String s) {
-        return new TranslatableComponent(s);
+        return Component.translatable(s);
     }
 }
