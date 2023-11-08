9f2b7adcffbf046f318a3844aa62061864b81a7f
SettingDust <settingdust@gmail.com>
2023-11-08 18:36:20 +0800

    fix: make forge port work
    close https://github.com/SettingDust/keymap/pull/1

--- forge/src/main/java/com/github/einjerjar/mc/keymapforge/cross/services/ForgeKeybindHelper.java
+++ forge/src/main/java/com/github/einjerjar/mc/keymapforge/cross/services/ForgeKeybindHelper.java
@@ -3,20 +3,18 @@ package com.github.einjerjar.mc.keymapforge.cross.services;
 import com.github.einjerjar.mc.keymap.cross.services.IKeybindHelper;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.ClientRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.Options;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class ForgeKeybindHelper implements IKeybindHelper {
 
-    @Override public KeyMapping create(InputConstants.Type type, int code, String name, String category) {
-        KeyMapping k = new KeyMapping(
-                name,
-                type,
-                code,
-                category
-        );
+    @Override
+    public KeyMapping create(InputConstants.Type type, int code, String name, String category) {
+        KeyMapping k = new KeyMapping(name, type, code, category);
 
-        ClientRegistry.registerKeyBinding(k);
-
+        Options options = Minecraft.getInstance().options;
+        options.keyMappings = ArrayUtils.add(options.keyMappings, k);
         return k;
     }
 }
